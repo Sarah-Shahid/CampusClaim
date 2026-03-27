1e7ad4d1659a192ca547a30e0e0e032da6f98e3f
Sarah Shahid <sarahshahid2006@gmail.com>
2026-03-28 01:24:29 +0500

    Added the first classes.

---
+++ User.java
@@ -0,0 +1,6 @@
+//every person who uses the application would first have to register
+//store all user info using this class. also used for file handling.
+
+public class User {
+    private String name;
+}
